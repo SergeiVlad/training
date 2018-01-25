d289dd9d1987957fe16f90f13f27acefeaf393e1
Sergei G <dneprfk@gmail.com>
2018-01-25 16:40:57 +0200

    loadtext training

--- study_numpy.py
+++ study_numpy.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = np.loadtxt('test5.txt')
+# A = np.loadtxt('test0.txt', skiprows = 1, converters = {6: lambda x: int(x, 16)})
+# A = np.loadtxt('test0.txt', skiprows = 1, usecols = (0,1,2,3,4))
+# A = np.loadtxt('test0.txt', skiprows = 1, usecols = (0,1,2,3,4,5), unpack = True)
+hex2int = lambda x: int(x, 16)
+A = np.loadtxt('test0.txt', skiprows = 1, converters = {6: hex2int})
 
-plt.plot(A[:,2])
+plt.plot(A)
 plt.grid(True)
-
 plt.show()
